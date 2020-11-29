8f00af595f81f79f4fdb7a5579c50fe91780ca54
Ray'J'D <Dike4mii4real@gmail.com>
2020-11-29 13:34:51 +0100

    removed logger

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,6 +1,5 @@
 import { createStore, applyMiddleware, compose } from "redux";
 import thunk from "redux-thunk";
-import logger from "redux-logger";
 import rootReducer from "./reducers/rootReducer";
 import { persistStore, persistReducer } from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
